a12192209f84c2bb235e306df9d5ff6ef7570bee
safimiazi <mohibullamiazi@gmail.com>
2024-01-29 14:06:12 +0600

    footer completed

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,10 +1,69 @@
-import React from 'react';
+import { Box, Button, Container, IconButton, Stack, Typography } from '@mui/material';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import Link from 'next/link';
 
 const Footer = () => {
+    const pages = [
+        {
+            route: "Home",
+            pathname: "/"
+        },
+        {
+            route: "Pages",
+            pathname: "/pages"
+        },
+        {
+            route: "Category",
+            pathname: "/category"
+        },
+        {
+            route: "News",
+            pathname: "/news"
+        },
+        {
+            route: "About",
+            pathname: "/about"
+        },
+        {
+            route: "Contact",
+            pathname: "/contact"
+        },
+    ];
     return (
-        <div>
-            
-        </div>
+        <Box className="bg-black px-2 py-2" direction={"row"} sx={{
+            "& svg": {
+                color: "white"
+            }
+        }}>
+            <Container>
+                <Box className='w-full text-center'>
+                    <IconButton>
+                        <FacebookIcon />
+                    </IconButton>
+                    <IconButton>
+                        <FacebookIcon />
+                    </IconButton>
+                    <IconButton>
+                        <FacebookIcon />
+                    </IconButton>
+                    <IconButton>
+                        <FacebookIcon />
+                    </IconButton>
+                </Box>
+                <Box className="text-center">
+                    {pages.map((page) => (
+                        <Link key={page} href={page.pathname}>
+                            <Button className='text-white'>
+                                {page.route}
+                            </Button>
+                        </Link>
+                    ))}
+                </Box>
+                <Typography className='text-white text-center'>
+                    @2023 the dragon news. design by mohibulla
+                </Typography>
+            </Container>
+        </Box>
     );
 };
 
